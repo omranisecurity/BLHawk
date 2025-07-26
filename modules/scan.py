f2b4cd60ec5987cf0780939a842f2db0097f8463
omranisecurity <mr.omrani81@gmail.com>
2025-07-26 10:36:30 +0330

    feat: add Buy Me a Coffee service to vulnerability check

--- modules/scan.py
+++ modules/scan.py
@@ -27,6 +27,10 @@ SERVICES = {
         "domains": ["play.google.com"],
         "check": lambda resp: resp.status_code == 404,
     },
+    "Buy Me a Coffee": {
+        "domains": ["buymeacoffee.com"],
+        "check": lambda resp: resp.status_code == 404,
+    },
 }
 
 def get_service_by_host(host):
