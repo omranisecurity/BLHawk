85a29b00705140a3ed37b90203a85ad7de1ff606
Mohammad Reza Omrani <38619429+omranisecurity@users.noreply.github.com>
2025-09-09 15:33:14 +0000

    chore: remove obsolete vulnerable test files from the repository

--- test_sources/vuln_md.md
+++
@@ -1,4 +0,0 @@
-# تست آسیب‌پذیری لینک‌ها
-
-[PyPI](https://pypi.org/project/nonexistent-package)
-[Dribbble](https://dribbble.com/nonexistentprofile)
--- test_sources/vuln_python.py
+++
@@ -1,5 +0,0 @@
-# نمونه فایل آسیب‌پذیر پایتون
-# لینک‌های آسیب‌پذیر
-url1 = "https://t.me/deadlink"
-url2 = "https://github.com/nonexistentuser/nonexistentrepo"
-url3 = "https://medium.com/@deleteduser"
